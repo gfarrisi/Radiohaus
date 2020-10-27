5dfe854f7e0d13fcf134010ca88c870cc96a7d60
Rohit <rohitpratti@gmail.com>
2020-10-27 15:07:11 -0700

    added hosting

--- radiohause-client/src/Routes/Home.js
+++ radiohause-client/src/Routes/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
+    height: '100vh',
+    backgroundColor: 'black',
+    display: 'grid',
+    placeItems: 'center',
   },
+  logo: {},
 }));
 
 export default function ContainedButtons() {
@@ -15,19 +16,13 @@ export default function ContainedButtons() {
 
   return (
     <div className={classes.root}>
-      <Button variant='contained'>Default</Button>
-      <Button variant='contained' color='primary'>
-        Primary
-      </Button>
-      <Button variant='contained' color='secondary'>
-        Secondary
-      </Button>
-      <Button variant='contained' disabled>
-        Disabled
-      </Button>
-      <Button variant='contained' color='primary' href='#contained-buttons'>
-        Link
-      </Button>
+      <div>
+        <img
+          className={classes.logo}
+          src='/images/mainLogo.png'
+          alt='main-logo'
+        />
+      </div>
     </div>
   );
 }
