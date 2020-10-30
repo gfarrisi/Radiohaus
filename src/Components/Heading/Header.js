1efd28271e94341bef417a7930ab7fb177caf3aa
Rohit <rohitpratti@gmail.com>
2020-10-29 20:16:34 -0700

    update to header

--- src/Components/Heading/Header.js
+++ src/Components/Heading/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import NavBar from './NavBar';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
   root: {
